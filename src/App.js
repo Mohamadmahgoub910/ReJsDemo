b502500ee7868c1d4ccb6bf6327352f54b259d3d
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2022-02-21 08:21:37 +0200

    make some updates create component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { StrictMode } from 'react'
+import Header from './components/Header'
+
+
 
 function App() {
+  const Title = 'Blog Post'
+  const Body = 'This is my blog post'
+  const Comments = [
+    { id: 1, text: 'Comment 1' },
+    { id: 2, text: 'Comment 2' },
+    { id: 3, text: 'Comment 3' }
+  ]
+  // const loading = false
+  const showComment = true
+  const commentBlock = (<ul>
+    {Comments.map((comment, index) => (
+      <li key={index} >{comment.text} </li>
+    ))}
+  </ul>)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StrictMode>
+      <>
+        <Header />
+      </>
+    </StrictMode>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import FeedBackItem from "./FeedBackItem";
+function Header({ text, bgColor, textColor }) {
+  const HeaderStyle = {
+    backgroundColor: bgColor,
+    color: textColor,
+    textAlign: "center",
+  };
+  return (
+    <>
+      <header style={HeaderStyle}>
+        <div className="container">
+          <p>FeedBack</p>
+        </div>
+      </header>
+      <FeedBackItem />
+    </>
+  );
+}
+
+Header.defaultProps = {
+  text: "FeedBackUi",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff9a95",
+};
+Header.propTypes = {
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+};
+
+export default Header;
